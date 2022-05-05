d52fb10a0d666ee20ef2ab1114fa0699da8b9fa5
Jose Alves <jose.sousalves@gmail.com>
2022-05-05 17:08:24 +0100

    style(text): update text component styles

--- packages/ui/components/Text/Text.styles.ts
+++ packages/ui/components/Text/Text.styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
+import { color, fontWeight, space, style, typography } from 'styled-system';
 
-export const Text = styled.p`
-  font-size: 16px;
+import { TextProps } from './Text.types';
+import { size } from './Text.variants';
+
+const textTransform = style({
+  key: 'textTransform',
+  cssProperty: 'textTransform',
+  prop: 'textTransform',
+});
+
+export const Text = styled.p<TextProps>`
+  ${size}
+  ${color}
+  ${typography}
+  ${space}
+  ${textTransform}
+  ${fontWeight}
 `;
