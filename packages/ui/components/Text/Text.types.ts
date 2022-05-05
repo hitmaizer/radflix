eeee12b727d5fdd86eed062c1abbff9f736f6123
Jose Alves <jose.sousalves@gmail.com>
2022-05-05 17:08:50 +0100

    refactor(text): update text component types file

--- packages/ui/components/Text/Text.types.ts
+++ packages/ui/components/Text/Text.types.ts
@@ -1,5 +1,12 @@
 import { ReactNode } from 'react';
 
-export interface TextProps {
-  children?: ReactNode;
+import { HeadingElement, TextElement } from '@styles/types';
+import { LayoutProps, SpaceProps, TypographyProps } from 'styled-system';
+
+export interface TextProps extends TypographyProps, SpaceProps, LayoutProps {
+  as?: HeadingElement | TextElement | 'div';
+  children: ReactNode;
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+  textTransform?: 'none' | 'uppercase' | 'lowercase' | 'capitalize';
+  color?: string;
 }
