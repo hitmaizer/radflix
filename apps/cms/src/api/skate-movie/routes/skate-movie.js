c0ccc944ee9544c62225c8836cb14266b737a5b4
Jose Alves <jose.sousalves@gmail.com>
2022-05-07 11:09:09 +0100

    feat(cms): adds skatemovie type and permissions

---
+++ apps/cms/src/api/skate-movie/routes/skate-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skate-movie router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::skate-movie.skate-movie');
