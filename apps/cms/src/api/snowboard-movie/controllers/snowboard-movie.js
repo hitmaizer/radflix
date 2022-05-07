a67e1865fb9a5c12bf949da41c27fd539448d157
Jose Alves <jose.sousalves@gmail.com>
2022-05-07 17:19:18 +0100

    feat(cms): add snowboard movies types

---
+++ apps/cms/src/api/snowboard-movie/controllers/snowboard-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  snowboard-movie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::snowboard-movie.snowboard-movie');
