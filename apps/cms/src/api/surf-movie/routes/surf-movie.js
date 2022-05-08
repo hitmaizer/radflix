8ed810de9632afff84e9d471c19762984b888718
Jose Alves <jose.sousalves@gmail.com>
2022-05-08 10:29:33 +0100

    feat(cms): add surf movies content type

---
+++ apps/cms/src/api/surf-movie/routes/surf-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * surf-movie router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::surf-movie.surf-movie');
