b2632c2c5ce3e03b3d461a966dfff1e64b966b49
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 11:58:46 +0100

    feat(logo): adds logo component markup file

---
+++ packages/ui/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import * as S from './Logo.styles';
+import { LogoProps } from './Logo.types';
+
+const Logo = ({ imgSrc }: LogoProps) => {
+  return <S.Logo src={imgSrc} width="167px" />;
+};
+
+export default Logo;
+
+Logo.defaultProps = {
+  imgSrc: 'assets/radflix-logo.svg',
+};
