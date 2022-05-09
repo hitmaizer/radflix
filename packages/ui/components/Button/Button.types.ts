ba983dbe655a8b367f7786dddc48811b3783c294
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 10:43:41 +0100

    feat(button): adds button component props types file

---
+++ packages/ui/components/Button/Button.types.ts
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+import {
+  BackgroundColorProps,
+  FlexboxProps,
+  LayoutProps,
+  PositionProps,
+  SpaceProps,
+} from 'styled-system';
+
+export interface ButtonProps
+  extends LayoutProps,
+    FlexboxProps,
+    SpaceProps,
+    PositionProps,
+    BackgroundColorProps {
+  children?: ReactNode;
+  borderRadius?: string;
+  outline?: boolean;
+  onClick?: () => void;
+  banner?: boolean;
+}
