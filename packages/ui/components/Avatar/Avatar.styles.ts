d64fe52449e2c0698ef0a562d839530eb5c63d0c
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 12:30:13 +0100

    feat(avatar): adds avatar component styles file

---
+++ packages/ui/components/Avatar/Avatar.styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { space, variant } from 'styled-system';
+
+import { AvatarProps } from './Avatar.types';
+
+export const size = variant({
+  prop: 'size',
+  variants: {
+    sm: {
+      width: '32px',
+    },
+    md: {
+      width: '60px',
+    },
+    lg: {
+      width: '80px',
+    },
+    xlg: {
+      width: '200px',
+    },
+  },
+});
+
+export const Avatar = styled.img<AvatarProps>`
+  ${space}
+  ${size}
+  border-radius: 8px;
+  cursor: pointer;
+`;
