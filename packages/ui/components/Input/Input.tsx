ad59f25d33ccb55b0b20a050f82dfee595461e8b
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 15:06:05 +0100

    feat(input): adds input component markup file

---
+++ packages/ui/components/Input/Input.tsx
@@ -0,0 +1,32 @@
+import Box from 'components/Box';
+import Button from 'components/Button';
+import Text from 'components/Text';
+import { NavigateNext } from 'styled-icons/material';
+
+import * as S from './Input.styles';
+import { InputProps } from './Input.types';
+
+const Input = ({ children, ...rest }: InputProps) => {
+  return (
+    <Box display="flex" flexDirection="row" {...rest}>
+      <S.Input type="text" placeholder="Email address" />
+      <Button
+        borderRadius="0px"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        padding="0 30px"
+      >
+        <Text size="lg">{children}</Text>
+        <NavigateNext size="32px" color="white" />
+      </Button>
+    </Box>
+  );
+};
+
+export default Input;
+
+Input.defaultProps = {
+  children: 'Get Started',
+};
