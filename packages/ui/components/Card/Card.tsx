21bbc99017eb0886e4f248ac786e3fce734564f5
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 12:40:21 +0100

    feat(card): adds card component markup file

---
+++ packages/ui/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import * as S from './Card.styles';
+import { CardProps } from './Card.types';
+
+const Card = ({ children, imgSrc, square, ...rest }: CardProps) => {
+  return (
+    <S.Card {...rest}>
+      <S.Image src={imgSrc} alt="placeholder" square={square} />
+      {children}
+    </S.Card>
+  );
+};
+
+export default Card;
+
+Card.defaultProps = {
+  imgSrc: '/assets/placeholder-card.jpg',
+  title: 'Seinfeld: S03E21 Something',
+  square: true,
+};
