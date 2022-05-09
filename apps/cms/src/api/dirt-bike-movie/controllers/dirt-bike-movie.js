74c0cdd36002f71b09d5b0340d503d917bca2024
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 18:55:09 +0100

    feat(cms): add dirt bike collection type

---
+++ apps/cms/src/api/dirt-bike-movie/controllers/dirt-bike-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  dirt-bike-movie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dirt-bike-movie.dirt-bike-movie');
---
+++ apps/cms/src/api/dirt-bike-movie/services/dirt-bike-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dirt-bike-movie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::dirt-bike-movie.dirt-bike-movie');
