26f08707515bb65720a05a6c0003d13d8b343017
Jose Alves <jose.sousalves@gmail.com>
2022-05-09 12:39:45 +0100

    feat(card): adds card component stories file

---
+++ packages/ui/components/Card/Card.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Card from './Card';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  args: {},
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
