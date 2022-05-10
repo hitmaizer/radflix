137297402240b097b99b7be83d19a110b8b6e8b4
Jose Alves <jose.sousalves@gmail.com>
2022-05-10 14:30:27 +0100

    feat(login ): adds login component stories file

---
+++ packages/ui/components/Login/Login.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Login from './Login';
+
+export default {
+  title: 'Components/Login',
+  component: Login,
+  args: {},
+} as ComponentMeta<typeof Login>;
+
+const Template: ComponentStory<typeof Login> = (args) => <Login {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
