d466e810a18ed865a08a3cb8e142601a396d5293
Jose Alves <jose.sousalves@gmail.com>
2022-05-11 17:05:48 +0100

    refactor(banner): banner component api call refactor includesloading

--- apps/web/src/components/Banner/Banner.tsx
+++ apps/web/src/components/Banner/Banner.tsx
@@ -6,6 +6,8 @@ import Stack from '@components/Stack';
 import Text from '@components/Text';
 import Link from 'next/link';
 
+import { Skeleton } from '@components';
+
 import axios from '../../axios/instance';
 import requests from '../../axios/requests';
 import * as S from './Banner.styles';
@@ -13,14 +15,25 @@ import { BannerProps, MovieObj } from './Banner.types';
 
 const Banner = ({ children, ...rest }: BannerProps) => {
   const [movie, setMovie] = useState<MovieObj>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
     async function fetchData() {
-      const req = await axios.get(requests.skateMovies);
-
-      setMovie(
-        req.data.data[Math.floor(Math.random() * req.data.data.length - 1)]
-      );
+      setLoading(true);
+      const req = await axios
+        .get(requests.skateMovies)
+        .then((res) =>
+          setMovie(
+            res.data.data[Math.floor(Math.random() * res.data.data.length - 1)]
+          )
+        )
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
 
       return req;
     }
@@ -29,30 +42,38 @@ const Banner = ({ children, ...rest }: BannerProps) => {
 
   return (
     <>
-      <S.Banner
-        {...rest}
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage: `linear-gradient(180deg, rgba(20,20,20,1) 9%, rgba(20,20,20,0) 51%, rgba(20,20,20,1) 100%), url("${movie?.attributes.backdropPoster}")`,
-          backgroundPosition: 'top center',
-        }}
-      >
-        <S.BannerContent>
-          <Heading color="white" fontWeight="bold">
-            {movie?.attributes.title}
-          </Heading>
-          {children}
-          <Stack display="flex" gridGap={4}>
-            <Link href={`/watch/${movie?.attributes.slug}`} passHref>
-              <Button banner> Play</Button>
-            </Link>
-            <Button banner>My List</Button>
-          </Stack>
-          <Text color="white" lineHeight={1.5} maxWidth="100px">
-            {`${movie?.attributes.description.substring(0, 150)}...`}
-          </Text>
-        </S.BannerContent>
-      </S.Banner>
+      {!loading && (
+        <S.Banner
+          {...rest}
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `linear-gradient(180deg, rgba(20,20,20,1) 9%, rgba(20,20,20,0) 51%, rgba(20,20,20,1) 100%), url("${movie?.attributes.backdropPoster}")`,
+            backgroundPosition: 'top center',
+          }}
+        >
+          <S.BannerContent>
+            <Heading color="white" fontWeight="bold">
+              {movie?.attributes.title}
+            </Heading>
+            {children}
+            <Stack display="flex" gridGap={4}>
+              <Link href={`/watch/${movie?.attributes.slug}`} passHref>
+                <Button banner> Play</Button>
+              </Link>
+              <Button banner>My List</Button>
+            </Stack>
+            <Text color="white" lineHeight={1.5} maxWidth="100px">
+              {`${movie?.attributes.description.substring(0, 150)}...`}
+            </Text>
+          </S.BannerContent>
+        </S.Banner>
+      )}
+      {loading && (
+        <>
+          <Skeleton banner />
+        </>
+      )}
+      {error && <p>{error}</p>}
     </>
   );
 };
