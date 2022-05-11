f2fa170449d9a83660169206a95725f9ef671bef
Jose Alves <jose.sousalves@gmail.com>
2022-05-11 20:37:11 +0100

    feat(content): adds content component markup file

---
+++ apps/web/src/components/Content/Content.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import requests from 'src/axios/requests';
+
+import Banner from '../Banner/Banner';
+import Row from '../Row/Row';
+import * as S from './Content.styles';
+import { ContentProps } from './Content.types';
+
+const Content = ({ children, ...rest }: ContentProps) => {
+  return (
+    <S.Content {...rest}>
+      <Banner />
+      <Row title="Skateboarding" fetchURL={requests.skateMovies} square />
+      <Row title="BMX Movies" fetchURL={requests.bmxMovies} square />
+      <Row title="Dirt Bikes" fetchURL={requests.dirtbikeMovies} square />
+      <Row title="Snowboarding" fetchURL={requests.snowboardMovies} square />
+      <Row title="Surf" fetchURL={requests.surfMovies} square />
+
+      {children}
+    </S.Content>
+  );
+};
+
+export default Content;
