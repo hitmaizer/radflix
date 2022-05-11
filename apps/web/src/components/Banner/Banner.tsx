2963171e5f3843184a36e647a5a8f91266a1c5f9
Jose Alves <jose.sousalves@gmail.com>
2022-05-11 15:52:19 +0100

    feat(banner): adds banner component markup file

---
+++ apps/web/src/components/Banner/Banner.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+
+import Button from '@components/Button';
+import Heading from '@components/Heading';
+import Stack from '@components/Stack';
+import Text from '@components/Text';
+import Link from 'next/link';
+
+import axios from '../../axios/instance';
+import requests from '../../axios/requests';
+import * as S from './Banner.styles';
+import { BannerProps, MovieObj } from './Banner.types';
+
+const Banner = ({ children, ...rest }: BannerProps) => {
+  const [movie, setMovie] = useState<MovieObj>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get(requests.skateMovies);
+
+      setMovie(
+        req.data.data[Math.floor(Math.random() * req.data.data.length - 1)]
+      );
+
+      return req;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <S.Banner
+        {...rest}
+        style={{
+          backgroundSize: 'cover',
+          backgroundImage: `linear-gradient(180deg, rgba(20,20,20,1) 9%, rgba(20,20,20,0) 51%, rgba(20,20,20,1) 100%), url("${movie?.attributes.backdropPoster}")`,
+          backgroundPosition: 'top center',
+        }}
+      >
+        <S.BannerContent>
+          <Heading color="white" fontWeight="bold">
+            {movie?.attributes.title}
+          </Heading>
+          {children}
+          <Stack display="flex" gridGap={4}>
+            <Link href={`/watch/${movie?.attributes.slug}`} passHref>
+              <Button banner> Play</Button>
+            </Link>
+            <Button banner>My List</Button>
+          </Stack>
+          <Text color="white" lineHeight={1.5} maxWidth="100px">
+            {`${movie?.attributes.description.substring(0, 150)}...`}
+          </Text>
+        </S.BannerContent>
+      </S.Banner>
+    </>
+  );
+};
+
+export default Banner;
