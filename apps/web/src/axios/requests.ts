5d336f7668295ecd9c1df94f3523967da8b40abd
Jose Alves <jose.sousalves@gmail.com>
2022-05-11 15:51:25 +0100

    feat(web): adds query params for different category movies

---
+++ apps/web/src/axios/requests.ts
@@ -0,0 +1,9 @@
+const requests = {
+  skateMovies: `/skate-movies`,
+  snowboardMovies: `/snowboard-movies`,
+  surfMovies: `/surf-movies`,
+  bmxMovies: `/bmx-movies`,
+  dirtbikeMovies: `/dirt-bike-movies`,
+};
+
+export default requests;
