53d6adefe400638d68b415c8551676fae3568e65
Jose Alves <jose.sousalves@gmail.com>
2022-05-11 14:34:02 +0100

    feat(browse): browse initial markup + protected route

--- apps/web/pages/browse/index.tsx
+++ apps/web/pages/browse/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import { GetServerSideProps } from 'next';
+import { useSession, getSession } from 'next-auth/react';
+import Link from 'next/link';
+import Browse from 'src/components/Browse/Browse';
+
+import { Heading, Logged, Logo, Navbar, Stack, Text } from '@components';
 
 const index = () => {
-  return <div>index</div>;
+  const { data: session } = useSession();
+
+  if (typeof window === 'undefined') return null;
+
+  if (session) {
+    return (
+      <>
+        <Browse>
+          <Navbar browse>
+            <Logo imgSrc="radflix-logo.svg" />
+            <Stack
+              display="flex"
+              alignItems="center"
+              gridGap="16px"
+              flex={1}
+              ml={8}
+            >
+              <Link href="/categories" passHref>
+                <Text color="white">Categorias</Text>
+              </Link>
+              <Link href="/documentaries" passHref>
+                <Text color="white">Documentários</Text>
+              </Link>
+              <Link href="/highlights" passHref>
+                <Text color="white">Highlights</Text>
+              </Link>
+            </Stack>
+            <Logged
+              imgSrc="/placeholder-avatar.jpg"
+              display="flex"
+              alignItems="center"
+            />
+          </Navbar>
+        </Browse>
+      </>
+    );
+  }
+  return <Heading>Please Login to access this page.</Heading>;
 };
 
 export default index;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
