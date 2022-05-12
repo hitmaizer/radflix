7eab827ac3e723bdcc2be405abe18c90f4b58d5c
Jose Alves <jose.sousalves@gmail.com>
2022-05-12 17:30:46 +0100

    feat(loading): adds loading component styles file

---
+++ packages/ui/components/Loading/Loading.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { flexbox, layout, space } from 'styled-system';
+
+export const Loading = styled.div`
+  ${space}
+  ${flexbox}
+  ${layout}
+
+  min-width: 100vw;
+  min-height: 100vh;
+`;
