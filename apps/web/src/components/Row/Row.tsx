8ee0c8e06d5f5fedbab600a1a9a9a410c9a346a8
Jose Alves <jose.sousalves@gmail.com>
2022-05-12 16:29:08 +0100

    refactor(row): removes local state adds loading + error from RTK

--- apps/web/src/components/Row/Row.tsx
+++ apps/web/src/components/Row/Row.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 
 import Card from '@components/Card';
 import Heading from '@components/Heading';
+import { useSelector, useDispatch } from 'react-redux';
+import { setError } from 'src/redux/error';
+import { setLoading } from 'src/redux/loading';
+import { RootState } from 'src/redux/store';
 import { SwiperSlide } from 'swiper/react';
-
-import { Skeleton, Stack } from '@components';
 
 import axios from '../../axios/instance';
 import { MovieObj } from '../Banner/Banner.types';
@@ -17,8 +19,9 @@ const Row = ({ title, fetchURL, square, poster, ...rest }: RowProps) => {
   const [movies, setMovies] = useState<MovieObj[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<MovieObj>();
   const [showUnder, setShowUnder] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
+  const error = useSelector((state: RootState) => state.error.error);
+  const loading = useSelector((state: RootState) => state.loading.loading);
+  const dispatch = useDispatch();
 
   const handleClick = (m: MovieObj) => {
     setSelectedMovie(m);
@@ -27,12 +30,12 @@ const Row = ({ title, fetchURL, square, poster, ...rest }: RowProps) => {
 
   useEffect(() => {
     async function fetchData() {
-      setLoading(true);
+      dispatch(setLoading(true));
       const request = await axios
         .get(fetchURL)
         .then((res) => setMovies(res.data?.data))
-        .catch((err) => setError(err))
-        .finally(() => setLoading(false));
+        .catch((err) => dispatch(setError(err)))
+        .finally(() => dispatch(setLoading(false)));
       return request;
     }
     fetchData();
@@ -40,55 +43,39 @@ const Row = ({ title, fetchURL, square, poster, ...rest }: RowProps) => {
 
   return (
     <>
-      <S.Row
-        display="flex"
-        flexDirection="column"
-        ml={16}
-        square={square}
-        poster={poster}
-        {...rest}
-      >
-        <Heading size="2xl" fontWeight="700" color="white" mr="auto">
-          {title}
-        </Heading>
-        <S.StyledSwiper slidesPerView={square ? 5 : 7} spaceBetween={8}>
-          {movies.map((movie: MovieObj) => (
-            <SwiperSlide key={movie.id} onClick={() => handleClick(movie)}>
-              <Card
-                poster={poster}
-                square={square}
-                title={movie.attributes.title}
-                imgSrc={
-                  square
-                    ? movie.attributes.backdropPoster
-                    : movie.attributes.moviePoster
-                }
-              />
-            </SwiperSlide>
-          ))}
-        </S.StyledSwiper>
-      </S.Row>
-      {showUnder && <UnderBanner selectedMovie={selectedMovie} />}
-      {loading && (
-        <>
-          <Stack
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="flex-start"
-          >
-            <Skeleton heading />
-            <Stack display="flex" gridGap="8px">
-              <Skeleton card />
-              <Skeleton card />
-              <Skeleton card />
-              <Skeleton card />
-              <Skeleton card />
-              <Skeleton card />
-            </Stack>
-          </Stack>
-        </>
+      {!loading && (
+        <S.Row
+          display="flex"
+          flexDirection="column"
+          ml={16}
+          square={square}
+          poster={poster}
+          {...rest}
+        >
+          <Heading size="2xl" fontWeight="700" color="white" mr="auto">
+            {title}
+          </Heading>
+          <S.StyledSwiper slidesPerView={square ? 5 : 7} spaceBetween={8}>
+            {movies.map((movie: MovieObj) => (
+              <SwiperSlide key={movie.id} onClick={() => handleClick(movie)}>
+                <Card
+                  poster={poster}
+                  square={square}
+                  title={movie.attributes.title}
+                  imgSrc={
+                    square
+                      ? movie.attributes.backdropPoster
+                      : movie.attributes.moviePoster
+                  }
+                />
+              </SwiperSlide>
+            ))}
+          </S.StyledSwiper>
+        </S.Row>
       )}
+
+      {showUnder && <UnderBanner selectedMovie={selectedMovie} />}
+
       {error && <p>{error}</p>}
     </>
   );
