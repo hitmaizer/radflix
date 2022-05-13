b9bc9291c87640ec3b2dfb4116c49d74dee56877
Jose Alves <jose.sousalves@gmail.com>
2022-05-13 18:15:36 +0100

    feat(cms): updates findOne method id to slug on all movies

--- apps/cms/src/api/all-movie/controllers/all-movie.js
+++ apps/cms/src/api/all-movie/controllers/all-movie.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::all-movie.all-movie');
+module.exports = createCoreController(
+  'api::all-movie.all-movie',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi
+        .service('api::all-movie.all-movie')
+        .find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
