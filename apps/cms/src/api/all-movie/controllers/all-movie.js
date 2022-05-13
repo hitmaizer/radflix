2b648f2e57a580d84e36ad17746f6778ab1ce123
Jose Alves <jose.sousalves@gmail.com>
2022-05-13 15:51:44 +0100

    refactor(cms): updates movie collection to all-movie collection

---
+++ apps/cms/src/api/all-movie/controllers/all-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  all-movie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::all-movie.all-movie');
