10076eb323000acd457a754bd5ae2ba66ff1b86b
Jose Alves <jose.sousalves@gmail.com>
2022-05-13 15:43:03 +0100

    refactor(cms): rf findOne method id to slug on all content types

--- apps/cms/src/api/movie/controllers/movie.js
+++ apps/cms/src/api/movie/controllers/movie.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::movie.movie');
+module.exports = createCoreController('api::movie.movie', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::movie.movie').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
