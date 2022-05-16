576a4accfa1d388493409e38dd4cb796c6f11241
Jose Alves <jose.sousalves@gmail.com>
2022-05-16 15:43:02 +0100

    style(content): adds flex gap responsive values

--- apps/web/src/components/Content/Content.styles.ts
+++ apps/web/src/components/Content/Content.styles.ts
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
+import { mediaQueries } from '@styles';
+
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
   margin: auto;
-  gap: 32px;
+  gap: 16px;
+  overflow: hidden;
+  ${mediaQueries.lg} {
+    gap: 32px;
+  }
 `;
