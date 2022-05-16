891e953d3aed7555a528faff492e00c0d84868d9
Jose Alves <jose.sousalves@gmail.com>
2022-05-16 15:51:40 +0100

    style(input): updates input responsive values

--- packages/ui/components/Input/Input.styles.ts
+++ packages/ui/components/Input/Input.styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import { flexbox, layout, position, space } from 'styled-system';
 
+import { mediaQueries } from '@styles';
+
 export const Input = styled.input`
   ${flexbox}
   ${layout}
   ${space}
   ${position}
   height: 70px;
-  min-width: 500px;
+  min-width: 150px;
   text-indent: 10px;
   border: solid 1px #8c8c8c;
   &::placeholder {
     font-size: medium;
+  }
+  ${mediaQueries.lg} {
+    min-width: 500px;
   }
 `;
