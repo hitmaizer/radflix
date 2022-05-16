ed3d44db325a647aca676021cb02c5d8e2ebd4e6
Jose Alves <jose.sousalves@gmail.com>
2022-05-16 11:14:17 +0100

    feat(web): adds index exports file to src components

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Banner } from './Banner';
+export { default as Browse } from './Browse';
+export { default as Content } from './Content';
+export { default as Homepage } from './Homepage';
+export { default as Row } from './Row';
+export { default as UnderBanner } from './UnderBanner';
