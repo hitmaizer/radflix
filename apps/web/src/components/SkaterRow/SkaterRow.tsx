8174854a015192409c275fe0d8e987f1e1e337f6
Jose Alves <jose.sousalves@gmail.com>
2022-05-19 16:37:12 +0100

    feat(skaterRow): adds SkaterRow component markup file

---
+++ apps/web/src/components/SkaterRow/SkaterRow.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { setError } from 'src/redux/error';
+import { setLoading } from 'src/redux/loading';
+import { RootState } from 'src/redux/store';
+import { SwiperSlide } from 'swiper/react';
+
+import { Card, Heading } from '@ui';
+
+import axios from '../../axios/instance';
+import UnderBanner from '../UnderBanner/UnderBanner';
+import * as S from './SkaterRow.styles';
+import { SkaterObj, SkaterRowProps } from './SkaterRow.types';
+import 'swiper/css';
+
+const Row = ({ title, fetchURL, square, poster, ...rest }: SkaterRowProps) => {
+  const [skaters, setSkaters] = useState<SkaterObj[]>([]);
+  const [selectedSkater, setSelectedSkater] = useState<SkaterObj>();
+  const [showUnder, setShowUnder] = useState<boolean>(false);
+  const error = useSelector((state: RootState) => state.error.error);
+  const loading = useSelector((state: RootState) => state.loading.loading);
+  const dispatch = useDispatch();
+
+  const handleClick = (m: SkaterObj) => {
+    setSelectedSkater(m);
+    setShowUnder(() => !showUnder);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      dispatch(setLoading(true));
+      const request = await axios
+        .get(fetchURL)
+        .then((res) => setSkaters(res.data?.data))
+        .catch((err) => dispatch(setError(err)))
+        .finally(() => dispatch(setLoading(false)));
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  return (
+    <>
+      {!loading && (
+        <S.Row
+          display="flex"
+          flexDirection="column"
+          ml={16}
+          square={square}
+          poster={poster}
+          {...rest}
+        >
+          <Heading size="2xl" fontWeight="700" color="white" mr="auto">
+            {title}
+          </Heading>
+          <S.StyledSwiper
+            slidesPerView={square ? 2 : 2}
+            spaceBetween={8}
+            breakpoints={{
+              1400: {
+                slidesPerView: square ? 5 : 7,
+              },
+              1024: {
+                slidesPerView: square ? 4 : 6,
+              },
+              768: {
+                slidesPerView: square ? 3 : 5,
+              },
+              576: {
+                slidesPerView: square ? 3 : 4,
+              },
+            }}
+          >
+            {skaters.map((skater: SkaterObj) => (
+              <SwiperSlide key={skater.id} onClick={() => handleClick(skater)}>
+                <Card
+                  poster={poster}
+                  square={square}
+                  title={skater.name}
+                  imgSrc={
+                    square ? skater.backdrop.data.url : skater.poster.data.url
+                  }
+                />
+              </SwiperSlide>
+            ))}
+          </S.StyledSwiper>
+        </S.Row>
+      )}
+
+      {showUnder && <UnderBanner selectedSkater={selectedSkater} />}
+
+      {error && <p>{error}</p>}
+    </>
+  );
+};
+
+export default Row;
