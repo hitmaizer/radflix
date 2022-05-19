11fab077894f01e71fab9be9b004235d84740776
Jose Alves <jose.sousalves@gmail.com>
2022-05-19 12:16:50 +0100

    feat(cms): es6 syntax for auth secret

--- apps/cms/config/server.js
+++ apps/cms/config/server.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   url: process.env.HEROKU_URL,
   admin: {
     auth: {
-      secret: process.env.JWT_SECRET,
+      secret: `${process.env.JWT_SECRET}`,
     },
   },
 });
