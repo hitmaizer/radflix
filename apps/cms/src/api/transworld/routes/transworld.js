440fe7f8052c21aa5561448ce783f581c6e76135
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 12:25:51 +0100

    feat(cms): adds transworld content type files

---
+++ apps/cms/src/api/transworld/routes/transworld.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * transworld router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::transworld.transworld');
