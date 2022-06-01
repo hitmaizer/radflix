8e8dd026cf3e6d24e3be3c7458e2ea1489946fd4
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 12:31:44 +0100

    feat(cms): adds berric content type files

---
+++ apps/cms/src/api/berric/controllers/berric.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  berric controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::berric.berric');
---
+++ apps/cms/src/api/berric/services/berric.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * berric service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::berric.berric');
