7cb59aed4de1ad0295cce82bd2e307f76d7b6850
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 12:09:13 +0100

    feat(cms): adds snowboard-doc content type files

---
+++ apps/cms/src/api/snowboard-doc/controllers/snowboard-doc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  snowboard-doc controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::snowboard-doc.snowboard-doc');
