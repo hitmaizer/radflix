695839a99e347db76cbb9d15772ea4bcebffacd7
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 17:20:59 +0100

    refactor(cms): updates surf-doc findONe to search by slug

--- apps/cms/src/api/surf-doc/controllers/surf-doc.js
+++ apps/cms/src/api/surf-doc/controllers/surf-doc.js
@@ -1,9 +1,21 @@
-'use strict';
-
 /**
  *  surf-doc controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::surf-doc.surf-doc');
+module.exports = createCoreController(
+  'api::surf-doc.surf-doc',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi.service('api::surf-doc.surf-doc').find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
