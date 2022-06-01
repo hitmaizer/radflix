36c89036e4e7228569a6f4d0b7b0aa950247eb6b
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 17:20:21 +0100

    refactor(cms): updates skate-doc findOne to search by slug

--- apps/cms/src/api/skate-doc/controllers/skate-doc.js
+++ apps/cms/src/api/skate-doc/controllers/skate-doc.js
@@ -1,9 +1,23 @@
-'use strict';
-
 /**
  *  skate-doc controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::skate-doc.skate-doc');
+module.exports = createCoreController(
+  'api::skate-doc.skate-doc',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi
+        .service('api::skate-doc.skate-doc')
+        .find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
