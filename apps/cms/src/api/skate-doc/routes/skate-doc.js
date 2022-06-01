2b2581df10a7a89452ed8f4c6494b54364b2a714
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 11:56:49 +0100

    feat(cms): adds skate-doc collection type files

---
+++ apps/cms/src/api/skate-doc/routes/skate-doc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skate-doc router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::skate-doc.skate-doc');
