225ab955f1a1abfa05ed7ef6120880f0e3c89504
Jose Alves <jose.sousalves@gmail.com>
2022-06-01 11:56:30 +0100

    feat(cms): adds surf-doc content type files

---
+++ apps/cms/src/api/surf-doc/services/surf-doc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * surf-doc service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::surf-doc.surf-doc');
