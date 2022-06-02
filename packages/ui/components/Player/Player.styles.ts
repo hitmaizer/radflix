48ad478e7a11bd2c3e09c3d6246f2f281cf40266
Jose Alves <jose.sousalves@gmail.com>
2022-06-02 17:08:42 +0100

    feat(Player): deletes old BackBtn from player component

--- packages/ui/components/Player/Player.styles.ts
+++ packages/ui/components/Player/Player.styles.ts
@@ -1,4 +1,3 @@
-import { ArrowBack } from '@styled-icons/evaicons-solid/ArrowBack';
 import styled from 'styled-components';
 
 export const Player = styled.iframe`
@@ -8,17 +7,4 @@ export const Player = styled.iframe`
 
 export const PlayerContainer = styled.div`
   position: relative;
-`;
-
-export const BackBtn = styled(ArrowBack)`
-  cursor: pointer;
-  position: absolute;
-  top: 5%;
-  left: 5%;
-  width: 80px;
-  color: white;
-  transition: all 300ms ease;
-  &:hover {
-    transform: scale(1.1);
-  }
 `;
