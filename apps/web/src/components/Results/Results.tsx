802e3c6fc8447a75958228a95696c50120f9009b
Jose Alves <jose.sousalves@gmail.com>
2022-06-04 15:22:46 +0100

    feat(Results): adds Results component files

---
+++ apps/web/src/components/Results/Results.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import ImageCard from '@components/ImageCard';
+import { StyledSwiper } from '@components/SkaterRow/SkaterRow.styles';
+import { SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import { Heading } from '@ui';
+
+import * as S from './Results.styles';
+import { ResultsProps } from './Results.types';
+
+const Results = ({ children, data, square, ...rest }: ResultsProps) => {
+  return (
+    <S.Results {...rest}>
+      <Heading size="2xl" fontWeight="700" color="white" mr="auto">
+        We found {data?.length} results
+      </Heading>
+      <StyledSwiper
+        slidesPerView={square ? 2 : 2}
+        spaceBetween={8}
+        breakpoints={{
+          1400: {
+            slidesPerView: square ? 5 : 7,
+          },
+          1024: {
+            slidesPerView: square ? 4 : 6,
+          },
+          768: {
+            slidesPerView: square ? 3 : 5,
+          },
+          576: {
+            slidesPerView: square ? 3 : 4,
+          },
+        }}
+      >
+        {data?.map((value) => {
+          return (
+            <SwiperSlide key={value.id}>
+              <ImageCard imgSrc={value.backdropPoster} square />
+            </SwiperSlide>
+          );
+        })}
+      </StyledSwiper>
+      {children}
+    </S.Results>
+  );
+};
+
+export default Results;
