a73b8b090bb961cc01ba9c6efded1e1d0c8df064
Jose Alves <jose.sousalves@gmail.com>
2022-06-04 15:16:45 +0100

    feat(logged): updates logged location to next app

--- packages/ui/components/Logged/Logged.types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-
-import { FlexboxProps, LayoutProps, SpaceProps } from 'styled-system';
-
-export interface LoggedProps extends FlexboxProps, SpaceProps, LayoutProps {
-  children?: ReactNode;
-  imgSrc: string | undefined;
-}
