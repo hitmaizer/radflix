4129758c675d260fc10e5e0a2fde8ed58e453d07
Jose Alves <jose.sousalves@gmail.com>
2022-06-04 12:45:23 +0100

    feat(Logged): updates render of logged component adds show state

--- packages/ui/components/Logged/Logged.tsx
+++ packages/ui/components/Logged/Logged.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import * as HoverCard from '@radix-ui/react-hover-card';
 import { Pencil, QuestionCircle } from '@styled-icons/bootstrap';
 
@@ -7,9 +9,20 @@ import * as S from './Logged.styles';
 import { LoggedProps } from './Logged.types';
 
 const Logged = ({ children, imgSrc, ...rest }: LoggedProps) => {
+  const [showFilter, setShowFilter] = useState<boolean>(false);
+
   return (
     <S.Logged {...rest}>
-      <S.SearchIcon size="24px" color="white" />
+      <S.SearchIcon
+        size="24px"
+        color="white"
+        onClick={() => setShowFilter(!showFilter)}
+      />
+      {showFilter && (
+        <S.FilterContainer>
+          <S.Input placeholder="Search for movies" />
+        </S.FilterContainer>
+      )}
       <S.BellIcon size="24px" color="white" />
       <Avatar size="sm" imgSrc={imgSrc} />
       <HoverCard.Root openDelay={250} closeDelay={250}>
