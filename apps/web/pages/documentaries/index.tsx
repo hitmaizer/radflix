4ff3a37d676434e44fde5e91c79976c4346b89ca
Jose Alves <jose.sousalves@gmail.com>
2022-06-04 16:05:06 +0100

    feat(documentaries): fetches allDocs + renders results

--- apps/web/pages/documentaries/index.tsx
+++ apps/web/pages/documentaries/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import DocBanner from '@components/DocBanner';
 import DocRow from '@components/DocRow';
+import ResultsDocs from '@components/ResultsDocs';
 import { GetServerSideProps } from 'next';
 import { getSession, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'src/axios/instance';
 import requests from 'src/axios/requests';
+import { setDocs } from 'src/redux/docs';
+import { setError } from 'src/redux/error';
+import { setLoading } from 'src/redux/loading';
 import { RootState } from 'src/redux/store';
 
 import { Browse, Logged, MenuList } from '@components';
@@ -38,37 +42,29 @@ interface Doc {
 const index = ({ doc }: Doc) => {
   const { data: session } = useSession();
   const loading = useSelector((state: RootState) => state.loading.loading);
+  const docs = useSelector((state: RootState) => state.docsData.docs);
+  const filteredData = useSelector(
+    (state: RootState) => state.filteredData.filter
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchData() {
+      dispatch(setLoading(true));
+      axios
+        .get(requests.allDocs)
+        .then((res) => dispatch(setDocs(res.data?.data)))
+        .catch((err) => dispatch(setError(err)))
+        .finally(() => dispatch(setLoading(false)));
+    }
+    fetchData();
+  }, []);
 
   if (typeof window === 'undefined') return null;
   if (session) {
     return (
       <>
         <Browse>
-          <Navbar browse>
-            <Link href="/browse" passHref>
-              <a href="dummy">
-                <Logo imgSrc="radflix-logo.png" width="100px" />
-              </a>
-            </Link>
-            <MenuList />
-            <Logged
-              imgSrc="/placeholder-avatar.jpg"
-              display="flex"
-              alignItems="center"
-            >
-              <Link href="/" passHref>
-                <Button text onClick={() => signOut()}>
-                  <Text>Logout from Radflix</Text>
-                </Button>
-              </Link>
-            </Logged>
-          </Navbar>
-          <DocBanner
-            imgSrc={doc.backdrop.data[0].url}
-            title={doc.title}
-            slug={doc.slug}
-            description={doc.description}
-          />
           {loading && (
             <>
               <Loading
@@ -89,28 +85,61 @@ const index = ({ doc }: Doc) => {
               </Loading>
             </>
           )}
-          <Stack display="flex" vertical gridGap="40px">
-            <DocRow
-              title="Skateboarding Documentaries"
-              fetchURL={requests.skateDocs}
-              square
-            />
-            <DocRow
-              title="Surf Documentaries"
-              fetchURL={requests.surfDocs}
-              square
-            />
-            <DocRow
-              title="BMX Documentaries"
-              fetchURL={requests.bmxDocs}
-              square
-            />
-            <DocRow
-              title="Snowboard Documentaries"
-              fetchURL={requests.snowDocs}
-              square
-            />
-          </Stack>
+          <Navbar browse>
+            <Link href="/browse" passHref>
+              <a href="dummy">
+                <Logo imgSrc="radflix-logo.png" width="100px" />
+              </a>
+            </Link>
+            <MenuList />
+            <Logged
+              imgSrc="/placeholder-avatar.jpg"
+              display="flex"
+              alignItems="center"
+              filteredData={docs}
+            >
+              <Link href="/" passHref>
+                <Button text onClick={() => signOut()}>
+                  <Text>Logout from Radflix</Text>
+                </Button>
+              </Link>
+            </Logged>
+          </Navbar>
+          {filteredData.length !== 0 && (
+            <ResultsDocs data={filteredData} square />
+          )}
+          {filteredData.length === 0 && (
+            <>
+              <DocBanner
+                imgSrc={doc.backdrop.data[0].url}
+                title={doc.title}
+                slug={doc.slug}
+                description={doc.description}
+              />
+              <Stack display="flex" vertical gridGap="40px">
+                <DocRow
+                  title="Skateboarding Documentaries"
+                  fetchURL={requests.skateDocs}
+                  square
+                />
+                <DocRow
+                  title="Surf Documentaries"
+                  fetchURL={requests.surfDocs}
+                  square
+                />
+                <DocRow
+                  title="BMX Documentaries"
+                  fetchURL={requests.bmxDocs}
+                  square
+                />
+                <DocRow
+                  title="Snowboard Documentaries"
+                  fetchURL={requests.snowDocs}
+                  square
+                />
+              </Stack>
+            </>
+          )}
         </Browse>
       </>
     );
