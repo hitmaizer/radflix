a1b2ee52749f2ec809ade8415285b93f29ab3d71
Jose Alves <jose.sousalves@gmail.com>
2022-06-15 16:03:59 +0100

    refactor(cms): updates all-brand findOne method to search by slug

--- apps/cms/src/api/all-brand/controllers/all-brand.js
+++ apps/cms/src/api/all-brand/controllers/all-brand.js
@@ -1,9 +1,23 @@
-'use strict';
-
 /**
  *  all-brand controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::all-brand.all-brand');
+module.exports = createCoreController(
+  'api::all-brand.all-brand',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi
+        .service('api::all-brand.all-brand')
+        .find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
