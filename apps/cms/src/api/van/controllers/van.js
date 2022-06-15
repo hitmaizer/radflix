0d172fb5c40b9e4d775d6094de393c3598aa0a81
Jose Alves <jose.sousalves@gmail.com>
2022-06-15 16:04:30 +0100

    refactor(cms): updates vans findOne method to search by slug

--- apps/cms/src/api/van/controllers/van.js
+++ apps/cms/src/api/van/controllers/van.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  van controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::van.van');
+module.exports = createCoreController('api::van.van', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::van.van').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
