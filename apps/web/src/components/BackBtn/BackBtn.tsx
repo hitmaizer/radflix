c6ae07d761e54487774e0f83a525e0913567cbca
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 15:10:51 +0100

    refactor(BackBtn): forces movies state to refresh on BackBtn use

--- apps/web/src/components/BackBtn/BackBtn.tsx
+++ apps/web/src/components/BackBtn/BackBtn.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import { useRouter } from 'next/router';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useActive } from 'src/hooks/useActive';
+import { setMovies } from 'src/redux/allMovies';
 import { setFilteredData } from 'src/redux/filter';
+import { RootState } from 'src/redux/store';
 
 import * as S from './BackBtn.styles';
 import { BackBtnProps } from './BackBtn.types';
@@ -12,9 +14,11 @@ const BackBtn = ({ children, ...rest }: BackBtnProps) => {
   const router = useRouter();
   const active = useActive(4000);
   const dispatch = useDispatch();
+  const movies = useSelector((state: RootState) => state.movies.movies);
 
   const handleBack = () => {
     dispatch(setFilteredData([]));
+    dispatch(setMovies(movies));
     window.scrollTo(0, 0);
     router.back();
   };
