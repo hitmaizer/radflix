084c14542cb72c9f62a8f5c2290e3581454ead59
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 15:00:55 +0100

    refactor(BackBtn): clears filteredData on backBtn click

--- apps/web/src/components/BackBtn/BackBtn.tsx
+++ apps/web/src/components/BackBtn/BackBtn.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
 import { useActive } from 'src/hooks/useActive';
+import { setFilteredData } from 'src/redux/filter';
 
 import * as S from './BackBtn.styles';
 import { BackBtnProps } from './BackBtn.types';
@@ -9,10 +11,18 @@ import { BackBtnProps } from './BackBtn.types';
 const BackBtn = ({ children, ...rest }: BackBtnProps) => {
   const router = useRouter();
   const active = useActive(4000);
+  const dispatch = useDispatch();
+
+  const handleBack = () => {
+    dispatch(setFilteredData([]));
+    window.scrollTo(0, 0);
+    router.back();
+  };
+
   return (
     <>
       {active && (
-        <S.BackBtn {...rest} onClick={() => router.back()}>
+        <S.BackBtn {...rest} onClick={handleBack}>
           {children}
         </S.BackBtn>
       )}
