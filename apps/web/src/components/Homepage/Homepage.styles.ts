99065366290162e0a76c706c598a2cdd33fe1408
Jose Alves <jose.sousalves@gmail.com>
2022-06-26 12:22:36 +0100

    style(Homepage): adjusts Homepage component style rules

--- apps/web/src/components/Homepage/Homepage.styles.ts
+++ apps/web/src/components/Homepage/Homepage.styles.ts
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 import { flexbox, layout, position, space } from 'styled-system';
 
@@ -7,11 +8,15 @@ export const Homepage = styled.div`
   ${space}
   ${position}
   
-  background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.9) 0.01%, rgba(0, 0, 0, 0.2) 33.08%, rgba(0, 0, 0, 0.9) 100%),
-    url('home-banner.png');
+  background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.9) 0.01%, rgba(0, 0, 0, 0.2) 33.08%, rgba(0, 0, 0, 0.9) 100%);
   background-position: 50%;
   background-repeat: no-repeat;
   background-size: cover;
   min-height: 100vh;
   padding: 16px;
+  position: relative;
+`;
+
+export const StyledImage = styled(Image)`
+  object-fit: cover;
 `;
