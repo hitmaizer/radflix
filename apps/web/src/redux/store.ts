a4220ff5ade9873374b2cafbcc43b3436747f00b
Jose Alves <jose.sousalves@gmail.com>
2022-06-27 14:41:48 +0100

    feat(redux): updates RTK store file to include new slices

--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -2,14 +2,18 @@ import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 
 import moviesSlice from './allMovies';
 import bmxSlice from './bmx';
+import bmxDocsSlice from './bmxDocs';
 import dirtBikesSlice from './dirtBikes';
 import docsSlice from './docs';
 import errorSlice from './error';
 import filterSlice from './filter';
 import loadingSlice from './loading';
 import skateboardingSlice from './skateboarding';
+import skateDocsSlice from './skateDocs';
 import skatersSlice from './skaters';
 import snowboardingSlice from './snowboarding';
+import snowDocsSlice from './snowDocs';
+import surfDocsSlice from './surfDocs';
 import surfingSlice from './surfing';
 
 export const store = configureStore({
@@ -25,6 +29,10 @@ export const store = configureStore({
     surfing: surfingSlice,
     bmx: bmxSlice,
     dirtBikes: dirtBikesSlice,
+    skateDocs: skateDocsSlice,
+    snowDocs: snowDocsSlice,
+    bmxDocs: bmxDocsSlice,
+    surfDocs: surfDocsSlice,
   },
 });
 
