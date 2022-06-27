22dd064239c30ac32f5e58ba806b3cd2b81ea389
Jose Alves <jose.sousalves@gmail.com>
2022-06-27 14:37:55 +0100

    refactor(documentaries): updates documentaries page logic 2use StaticProps

--- apps/web/pages/documentaries/index.tsx
+++ apps/web/pages/documentaries/index.tsx
@@ -1,45 +1,39 @@
 import React, { useEffect } from 'react';
 
-import DocBanner from '@components/DocBanner';
-import DocRow from '@components/DocRow';
 import ResultsDocs from '@components/ResultsDocs';
-import { GetServerSideProps } from 'next';
-import { getSession, signOut, useSession } from 'next-auth/react';
+import { GetStaticProps } from 'next';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useSelector, useDispatch } from 'react-redux';
-import axios from 'src/axios/instance';
-import requests from 'src/axios/requests';
+import { setBmxDocs } from 'src/redux/bmxDocs';
 import { setDocs } from 'src/redux/docs';
-import { setError } from 'src/redux/error';
 import { setLoading } from 'src/redux/loading';
+import { setSkateDocs } from 'src/redux/skateDocs';
+import { setSnowDocs } from 'src/redux/snowDocs';
 import { RootState } from 'src/redux/store';
+import { setSurfDocs } from 'src/redux/surfDocs';
 
-import { Browse, Logged, MenuList } from '@components';
-import { Button, Loading, Logo, Navbar, Skeleton, Stack, Text } from '@ui';
+import {
+  Browse,
+  DocBanner,
+  Homepage,
+  Logged,
+  MenuList,
+  Row,
+} from '@components';
+import {
+  Box,
+  Button,
+  Heading,
+  Loading,
+  Logo,
+  Navbar,
+  Skeleton,
+  Stack,
+  Text,
+} from '@ui';
 
-interface Doc {
-  doc: {
-    backdrop: {
-      data: {
-        url: string;
-        blurhash: string;
-      };
-    };
-    id: number;
-    movieLink: string;
-    poster: {
-      data: {
-        url: string;
-        blurhash: string;
-      };
-    };
-    slug: string;
-    title: string;
-    description: string;
-  };
-}
-
-const index = ({ doc }: Doc) => {
+const index = ({ allDocs, skateDocs, snowDocs, surfDocs, bmxDocs }: any) => {
   const { data: session } = useSession();
   const loading = useSelector((state: RootState) => state.loading.loading);
   const docs = useSelector((state: RootState) => state.docsData.docs);
@@ -49,16 +43,14 @@ const index = ({ doc }: Doc) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function fetchData() {
-      dispatch(setLoading(true));
-      axios
-        .get(requests.allDocs)
-        .then((res) => dispatch(setDocs(res.data?.data)))
-        .catch((err) => dispatch(setError(err)))
-        .finally(() => dispatch(setLoading(false)));
-    }
-    fetchData();
-  }, []);
+    dispatch(setLoading(true));
+    dispatch(setDocs(allDocs));
+    dispatch(setSkateDocs(skateDocs));
+    dispatch(setSnowDocs(snowDocs));
+    dispatch(setSurfDocs(surfDocs));
+    dispatch(setBmxDocs(bmxDocs));
+    dispatch(setLoading(false));
+  }, [allDocs]);
 
   if (typeof window === 'undefined') return null;
   if (session) {
@@ -110,33 +102,31 @@ const index = ({ doc }: Doc) => {
           )}
           {filteredData.length === 0 && (
             <>
-              <DocBanner
-                imgSrc={doc.backdrop.data[0].url}
-                title={doc.title}
-                slug={doc.slug}
-                description={doc.description}
-                path="docs"
-              />
+              <DocBanner path="docs" docs={allDocs} />
               <Stack display="flex" vertical gridGap="40px">
-                <DocRow
+                <Row
                   title="Skateboarding Documentaries"
-                  fetchURL={requests.skateDocs}
+                  store={skateDocs}
                   square
+                  path="docs"
                 />
-                <DocRow
+                <Row
                   title="Surf Documentaries"
-                  fetchURL={requests.surfDocs}
+                  store={surfDocs}
                   square
+                  path="docs"
                 />
-                <DocRow
+                <Row
                   title="BMX Documentaries"
-                  fetchURL={requests.bmxDocs}
+                  store={bmxDocs}
                   square
+                  path="docs"
                 />
-                <DocRow
+                <Row
                   title="Snowboard Documentaries"
-                  fetchURL={requests.snowDocs}
+                  store={snowDocs}
                   square
+                  path="docs"
                 />
               </Stack>
             </>
@@ -145,30 +135,55 @@ const index = ({ doc }: Doc) => {
       </>
     );
   }
+  return (
+    <Homepage>
+      <Box
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        pt={15}
+      >
+        <Heading size="4xl" color="white" fontWeight="bold" textAlign="center">
+          Please Login to access this page.
+        </Heading>
+      </Box>
+    </Homepage>
+  );
 };
 
 export default index;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  const response = await axios.get(requests.allDocs);
-  const data = await response.data.data[
-    Math.floor(Math.random() * response.data.data.length)
-  ];
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
+export const getStaticProps: GetStaticProps = async () => {
+  const request1 = await fetch(
+    'https://radflix-cms.herokuapp.com/api/all-docs?populate=*'
+  );
+  const allDocs = await request1.json();
+  const request2 = await fetch(
+    'https://radflix-cms.herokuapp.com/api/skate-docs?populate=*'
+  );
+  const skateDocs = await request2.json();
+  const request3 = await fetch(
+    'https://radflix-cms.herokuapp.com/api/surf-docs?populate=*'
+  );
+  const surfDocs = await request3.json();
+  const request4 = await fetch(
+    'https://radflix-cms.herokuapp.com/api/bmx-docs?populate=*'
+  );
+  const bmxDocs = await request4.json();
+  const request5 = await fetch(
+    'https://radflix-cms.herokuapp.com/api/snowboard-docs?populate=*'
+  );
+  const snowDocs = await request5.json();
 
   return {
     props: {
-      session,
-      doc: data,
+      allDocs: allDocs.data,
+      skateDocs: skateDocs.data,
+      surfDocs: surfDocs.data,
+      bmxDocs: bmxDocs.data,
+      snowDocs: snowDocs.data,
     },
+    revalidate: 43200,
   };
 };
