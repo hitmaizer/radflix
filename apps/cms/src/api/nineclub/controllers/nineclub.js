4b5117d799db93f6de9d3417d9f8d60fff968d22
Jose Alves <jose.sousalves@gmail.com>
2022-06-28 12:26:49 +0100

    feat(cms): adds nineclub content type

---
+++ apps/cms/src/api/nineclub/controllers/nineclub.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  nineclub controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::nineclub.nineclub');
