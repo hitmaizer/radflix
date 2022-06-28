241f1bd0e4bc56b393c16e93fde51ae5dc1ab4e7
Jose Alves <jose.sousalves@gmail.com>
2022-06-28 12:27:02 +0100

    feat(cms): adds shredzshop content type

---
+++ apps/cms/src/api/shredzshop/controllers/shredzshop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  shredzshop controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shredzshop.shredzshop');
---
+++ apps/cms/src/api/shredzshop/services/shredzshop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shredzshop service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shredzshop.shredzshop');
