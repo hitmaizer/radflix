928d402c8f4b3cd2e01726246dbde13790328618
Jose Alves <jose.sousalves@gmail.com>
2022-06-28 12:33:30 +0100

    refactor(cms): updates nineclub and shredz findOne to slug

--- apps/cms/src/api/nineclub/controllers/nineclub.js
+++ apps/cms/src/api/nineclub/controllers/nineclub.js
@@ -1,9 +1,21 @@
-'use strict';
-
 /**
  *  nineclub controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::nineclub.nineclub');
+module.exports = createCoreController(
+  'api::nineclub.nineclub',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi.service('api::nineclub.nineclub').find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
