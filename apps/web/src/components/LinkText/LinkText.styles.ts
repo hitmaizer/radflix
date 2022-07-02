0ea5625f2f9975f253d7681cb6e3f6a9de2f3fa1
Jose Alves <jose.sousalves@gmail.com>
2022-07-02 14:26:26 +0100

    style(LinkText): updates LinkText basic styling

--- apps/web/src/components/LinkText/LinkText.styles.ts
+++ apps/web/src/components/LinkText/LinkText.styles.ts
@@ -6,6 +6,8 @@ export const LinkText = styled.a<LinkTextProps>`
   color: ${({ theme }) => theme.colors.white};
   font-weight: ${({ href, pathName }) => (href === pathName ? 'bold' : '400')};
   transition: color 300ms ease;
+  width: 100%;
+  text-align: center;
   &:hover {
     color: ${({ theme }) => theme.colors.gray300};
   }
