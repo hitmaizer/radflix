861faca9a6b4e46a0c92b6348131c6034ef2a43f
Jose Alves <jose.sousalves@gmail.com>
2022-07-29 19:58:30 +0100

    feat(cms): adds climbing content type files

---
+++ apps/cms/src/api/climbing/services/climbing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * climbing service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::climbing.climbing');
