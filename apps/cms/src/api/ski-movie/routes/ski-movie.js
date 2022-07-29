c32a03d9500462a9839cc6e6e67f0625d394268d
Jose Alves <jose.sousalves@gmail.com>
2022-07-29 19:59:41 +0100

    feat(cms): adds skiMovies content type files

---
+++ apps/cms/src/api/ski-movie/routes/ski-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ski-movie router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ski-movie.ski-movie');
---
+++ apps/cms/src/api/ski-movie/services/ski-movie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ski-movie service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ski-movie.ski-movie');
