a405c12827f4619397356eccaeb36efd10ca4adb
Jose Alves <jose.sousalves@gmail.com>
2022-07-29 19:58:06 +0100

    feat(cms): adds all-competition content type

---
+++ apps/cms/src/api/all-competition/routes/all-competition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * all-competition router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::all-competition.all-competition');
