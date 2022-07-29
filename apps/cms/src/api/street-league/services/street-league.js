1a1647cbdbd0e2a4773dace581ccd8827d124759
Jose Alves <jose.sousalves@gmail.com>
2022-07-29 19:59:59 +0100

    feat(cms): adds street league content type files

---
+++ apps/cms/src/api/street-league/services/street-league.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * street-league service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::street-league.street-league');
