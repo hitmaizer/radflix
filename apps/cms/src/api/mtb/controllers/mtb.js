8c72fe9e9e843e10bb4437da72c87dc884fb7884
Jose Alves <jose.sousalves@gmail.com>
2022-07-30 09:17:01 +0100

    refactor(cms): updates mtb findone method to slug

--- apps/cms/src/api/mtb/controllers/mtb.js
+++ apps/cms/src/api/mtb/controllers/mtb.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  mtb controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::mtb.mtb');
+module.exports = createCoreController('api::mtb.mtb', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::mtb.mtb').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
