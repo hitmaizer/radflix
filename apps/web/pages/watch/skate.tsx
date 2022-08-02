76efaac3b184f32f6e95092733303ee455b78194
Jose Alves <jose.sousalves@gmail.com>
2022-08-02 17:14:08 +0100

    feat(watch): adds skate route page file

--- apps/web/pages/watch/skate.tsx
+++ apps/web/pages/watch/skate.tsx
@@ -1,24 +1,65 @@
 import React, { useEffect, useState } from 'react';
 
-import { BackBtn } from '@components';
-import { Player, Stack } from '@ui';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading } from 'src/redux/loading';
+import { RootState } from 'src/redux/store';
+
+import { BackBtn, Browse } from '@components';
+import { Loading, Player, Skeleton, Stack } from '@ui';
 
 const YOUTUBE_PLAYLIST_ITEMS_API =
   'https://www.googleapis.com/youtube/v3/playlistItems';
 
 const skate = ({ data }) => {
   const [randomVideo, setRandomVideo] = useState();
+  const loading = useSelector((state: RootState) => state.loading.loading);
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(setLoading(true));
     const random = Math.floor(Math.random() * data.items.length);
     setRandomVideo(data.items[random].snippet.resourceId.videoId);
+    dispatch(setLoading(false));
   }, []);
 
   return (
-    <Stack>
-      <Player vidSrc={randomVideo} />
-      <BackBtn />
-    </Stack>
+    <>
+      {loading && (
+        <>
+          <Browse>
+            <Loading
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              ml={10}
+            >
+              <Skeleton heading />
+              <Stack display="flex" gridGap="16px">
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+              </Stack>
+              <Skeleton heading />
+              <Stack display="flex" gridGap="16px">
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+                <Skeleton card />
+              </Stack>
+            </Loading>
+          </Browse>
+        </>
+      )}
+      <Stack>
+        <Player vidSrc={randomVideo} />
+        <BackBtn />
+      </Stack>
+    </>
   );
 };
 
